ab8f28d393cf468c3949c88c1bd4f72f44844fbe
Marcos José <marcos06051988@gmail.com>
2021-09-14 06:29:26 -0300

    validacion

--- POO/herenciaMultiple/FiguraGeometrica.py
+++ POO/herenciaMultiple/FiguraGeometrica.py
@@ -1,7 +1,27 @@
 class FiguraGeometrica:
     def __init__(self, alto, ancho):
-        self._alto = alto
-        self._ancho = ancho
+        if self._validacion(alto):
+            self._alto = alto
+        else:
+            self._alto = 0
+            print(f'Valor fuera de rango {alto}')
+        if self._validacion(ancho):
+            self._ancho = ancho
+        else:
+            self._ancho = 0
+            print(f'Valor fuera de rango {ancho}') 
+
+    @property
+    def alto(self):
+        return self._alto
+
+    @alto.setter
+    def alto(self, alto):
+        if self._validacion(alto):
+            self._alto = alto
+        else:
+            print(f'Valor fuera de rango {alto}')
+
 
     @property
     def ancho(self):
@@ -9,15 +29,14 @@ class FiguraGeometrica:
 
     @ancho.setter
     def ancho(self, ancho):
-        self._ancho = ancho
+        if self._validacion(ancho):
+            self._ancho = ancho
+        else:
+            print(f'Valor fuera de rango {ancho}')
 
-    @property
-    def alto(self):
-        return self._alto
-
-    @alto.setter
-    def alto(self, ancho):
-        self._alto = alto
-
+    
     def __str__(self):
         return f'Figura geometrica: ancho: {self.alto} - alto: {self.ancho}'
+
+    def _validacion(self, valor):
+        return True if 1 < valor < 100 else False
