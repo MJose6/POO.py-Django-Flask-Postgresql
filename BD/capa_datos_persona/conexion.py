2c2969f338326f3966d4bae6fd5f528a7e01b616
Marcos José <marcos06051988@gmail.com>
2021-10-12 19:31:49 -0300

    practica, previo al laboratorio final de bd

---
+++ BD/capa_datos_persona/conexion.py
@@ -0,0 +1,60 @@
+from logger_base import log
+from psycopg2 import pool
+import sys
+
+class Conexion:
+    _DATABASE = 'test_db'
+    _USERNAME = 'postgres'
+    _PASSWORD = 'admin'
+    _DB_PORT = '5432'
+    _HOST = '127.0.0.1'
+    _MIN_CONEXIONES = 1
+    _MAX_CONEXIONES = 5
+    _pool = None
+
+    @classmethod
+    def obtenerPool(cls):
+        if cls._pool is None:
+            try:
+                cls._pool = pool.SimpleConnectionPool(cls._MIN_CONEXIONES, cls._MAX_CONEXIONES,
+                                                      host= cls._HOST,
+                                                      user= cls._USERNAME,
+                                                      password= cls._PASSWORD,
+                                                      port= cls._DB_PORT,
+                                                      database= cls._DATABASE)
+                log.debug(f'Creacion del pool exitosa: {cls._pool}')
+                return cls._pool
+            except Exception as e:
+                log.error(f'Ocurrio un error al obtener el pool: {e}')
+                sys.exit()
+        else:
+            return  cls._pool
+
+
+    @classmethod
+    def obtenerConexion(cls):
+        conexion = cls.obtenerPool().getconn()
+        log.debug(f'Conexion obtenida del pool: {conexion}')
+        return conexion
+
+    @classmethod
+    def liberarConexion(cls, conexion):
+        cls.obtenerPool().putconn(conexion)
+        log.debug(f'Regresamos la conexion al pool: {conexion}')
+
+    @classmethod
+    def cerrarConexiones(cls):
+        cls.obtenerPool().closeall()
+
+
+
+
+if __name__ == '__main__':
+    conexion1 = Conexion.obtenerConexion()
+    Conexion.liberarConexion(conexion1)
+    conexion2 = Conexion.obtenerConexion()
+    conexion3 = Conexion.obtenerConexion()
+    conexion4 = Conexion.obtenerConexion()
+    Conexion.liberarConexion(conexion4)
+    conexion5 = Conexion.obtenerConexion()
+    conexion6 = Conexion.obtenerConexion()
